124d106faa606506e84d383d9cd6cbc001df6e9e
krutr <krutrupera74@gmail.com>
2023-01-23 12:16:26 +0530

    checkin

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 
-import addImage from './add-image';
-import helloWorld from './hello-world';
-helloWorld();
-addImage();
+// import addImage from './add-image';
+// import helloWorld from './hello-world';
+// helloWorld();
+// addImage();
+
+import Heading from './components/heading/heading';
+import HelloWorldButton from './components/hello-world-button/hello-world-button';
+
+const heading = new Heading();
+const helloWorldButton = new HelloWorldButton();
+
+heading.render();
+helloWorldButton.render();
